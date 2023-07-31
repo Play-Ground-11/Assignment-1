9727df2962e7f6686bfcc6cfd4ed924795a082c0
Ganga Jayakodi <kpgangajayakodi@gmail.com>
2023-07-31 21:13:03 +0530

    complete task 5

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args) { 
+        int prefixElement = 978;
+        int groupElement = 3;
+        int registrantElement = 16;
+        int titleNumber = 1484100;
+
+        System.out.printf("\033[32;1m%s\033[30;0m-\033[33;1m%s\033[30;0m-\033[34;1m%s\033[30;0m-\033[35;1m%s\033[30;0m\n",prefixElement,groupElement,registrantElement,titleNumber);
+    }   
+}

3a5a867abbc805858129e2779f7e0b1e82975244
Ganga Jayakodi <kpgangajayakodi@gmail.com>
2023-07-31 21:08:22 +0530

    complete task 3

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int countryCode = 123;
+        int firstGroup = 456;
+        int secondGroup =7890;
+
+        System.out.printf("\033[34;1m(%s)\033[30;0m %s-%s\n",countryCode,firstGroup,secondGroup);
+    }
+    
+}

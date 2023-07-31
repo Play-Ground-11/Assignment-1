bcadcd6f52ec7c0f57a61b2f0d4b5eeabaca3128
Ganga Jayakodi <kpgangajayakodi@gmail.com>
2023-07-31 21:11:12 +0530

    complete task 4

---
+++ Demo4.java
@@ -0,0 +1,12 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        int year = 2023;
+        int month = 12;
+        int day = 28;
+        int hour = 23;
+        int minute = 59;
+        int second = 59;
+ 
+        System.out.printf("\033[32;1m%s\033[30;0m/\033[33;1m%s\033[30;0m/\033[34;1m%s\033[30;0m \033[35;1m%s\033[30;0m:\033[36;1m%s\033[30;0m:\033[37;1m%s\033[30;0m \n",year,month,day,hour,minute,second);
+     } 
+}
